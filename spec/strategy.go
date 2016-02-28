119ec4b8cb252a47a001b23b923a246198cb2795
Tim Schindler <tim@giantswarm.io>
2016-02-28 21:20:15 +0000

    made strategy public

---
+++ spec/strategy.go
@@ -0,0 +1,12 @@
+package spec
+
+// Strategy implements a container for a sequence of actions to be carried
+// around, e.g. by an impulse.
+type Strategy interface {
+	// String returns the string representation of the strategy's action
+	// sequence, e.g. "one,two,three".
+	String() string
+
+	// GetActions returns the ordered list of the strategy's action items.
+	GetActions() []ObjectType
+}
