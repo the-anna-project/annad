c4be719fce8c42654a09a443928e107f1c789315
Tim Schindler <tim@giantswarm.io>
2016-05-18 21:53:05 +0000

    added combination factory package

---
+++ factory/combination/combination.go
@@ -0,0 +1,8 @@
+// Package combination
+package combination
+
+//
+//     []interface{"CLG1", "CLG2", "CLG3"}
+//
+//     []interface{"string", "int", "[]float64"}
+//
---
+++ factory/factory.go
@@ -0,0 +1,3 @@
+// Package factory provides a collection of factory implementations. A factory
+// creates something on demand. Details can be found in the specific packages.
+package factory
