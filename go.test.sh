a27c42670dfe4456b6126c69ecb1f28591b7510e
Tim Schindler <tim@giantswarm.io>
2016-05-25 17:07:51 +0000

    debugging

--- go.test.sh
+++ go.test.sh
@@ -3,6 +3,9 @@
 set -e
 echo "" > coverage.txt
 
+pwd
+ls -lah
+
 for d in $(find ./* -maxdepth 10 -type f -name '*test.go' -not -path "./.workspace/*" -not -path "./.git/*" -exec dirname {} \; | sort -u); do
   go test -covermode=count -coverprofile=profile.out $d
   if [ -f profile.out ]; then
