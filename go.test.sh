b97226383b1618c894826f9b8177756a3d8d72a9
Tim Schindler <tim@giantswarm.io>
2016-05-25 17:00:17 +0000

    changed test mode to count

--- go.test.sh
+++ go.test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(find ./* -maxdepth 10 -type f -name '*test.go' -not -path "./.workspace/*" -not -path "./.git/*" -exec dirname {} \; | sort -u); do
-  go test -covermode=atomic -coverprofile=profile.out $d
+  go test -covermode=count -coverprofile=profile.out $d
   if [ -f profile.out ]; then
       cat profile.out >> coverage.txt
       rm profile.out
