876bb6f988ad6fe6699a73101601cccd40986ad6
Tim Schindler <tim@giantswarm.io>
2016-05-25 16:51:56 +0000

    add coveralls

--- go.test.sh
+++ go.test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(find ./* -maxdepth 10 -type f -name '*test.go' -not -path "./.workspace/*" -not -path "./.git/*" -exec dirname {} \; | sort -u); do
-  go test -coverprofile=profile.out -covermode=atomic $d
+  go test -covermode=atomic -coverprofile=profile.out $d
   if [ -f profile.out ]; then
       cat profile.out >> coverage.txt
       rm profile.out
