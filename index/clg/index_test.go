fa6b5c9eeef0a3e70c0d8d6e8a2b779a9e41151c
Tim Schindler <tim@giantswarm.io>
2016-05-26 23:02:17 +0000

    tests

--- index/clg/index_test.go
+++ index/clg/index_test.go
@@ -2,6 +2,7 @@ package clg
 
 import (
 	"testing"
+	"time"
 
 	"github.com/xh3b4sd/anna/spec"
 )
@@ -17,4 +18,21 @@ func testMaybeNewIndex(t *testing.T) spec.CLGIndex {
 	}
 
 	return newIndex
+}
+
+// Test_Index_Index_BootShutdown is a useful test on multiple levels. It
+// ensures that Boot and Shutdown work together, even when called multiple
+// times. Note on Boot we start a workerpool to generate CLG profiles. Thus a
+// lot of things are kicked off during this test. This increases the coverage
+// without testing any further specific detail.
+func Test_Index_Index_BootShutdown(t *testing.T) {
+	newIndex := testMaybeNewIndex(t)
+
+	go newIndex.Boot()
+	go newIndex.Boot()
+
+	time.Sleep(2 * time.Second)
+
+	newIndex.Shutdown()
+	newIndex.Shutdown()
 }
