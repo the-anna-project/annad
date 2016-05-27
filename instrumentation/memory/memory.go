4bdee768f37f778c7e0f787ad3fd4595571c77e5
Tim Schindler <tim@giantswarm.io>
2016-05-27 17:12:50 +0000

    comment

--- instrumentation/memory/memory.go
+++ instrumentation/memory/memory.go
@@ -1,3 +1,5 @@
-// Package memory implements spec.Instrumentation and provides
-// instrumentation primitives to manage application metrics.
+// Package memory mocks spec.Instrumentation and does effectively nothing. It
+// is only used for default configurations that require a satisfied
+// instrumentation implementation. This should then be overwritten with a valid
+// implementation if required.
 package memory
