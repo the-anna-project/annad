75a71b7f5099c7c6ed3fcd23d7bf43658dda1328
Tim Schindler <xh3b4sd@gmail.com>
2016-08-11 20:51:58 +0000

    updated CLGs

--- clg/subtract/generated.go
+++ clg/subtract/generated.go
@@ -25,6 +25,9 @@ type Config struct {
 	// Dependencies.
 	Log     spec.Log
 	Storage spec.Storage
+
+	// Settings.
+	InputChannel chan spec.NetworkPayload
 }
 
 // DefaultConfig provides a default configuration to create a new CLG object by
@@ -39,6 +42,9 @@ func DefaultConfig() Config {
 		// Dependencies.
 		Log:     log.NewLog(log.DefaultConfig()),
 		Storage: newStorage,
+
+		// Settings.
+		InputChannel: make(chan spec.NetworkPayload, 1000),
 	}
 
 	return newConfig
@@ -52,11 +58,17 @@ func New(config Config) (spec.CLG, error) {
 		Type:   ObjectType,
 	}
 
+	// Dependencies.
 	if newCLG.Log == nil {
 		return nil, maskAnyf(invalidConfigError, "logger must not be empty")
 	}
 	if newCLG.Storage == nil {
 		return nil, maskAnyf(invalidConfigError, "storage must not be empty")
+	}
+
+	// Settings.
+	if newCLG.InputChannel == nil {
+		return nil, maskAnyf(invalidConfigError, "input channel must not be empty")
 	}
 
 	newCLG.Log.Register(newCLG.GetType())
@@ -81,29 +93,39 @@ type clg struct {
 	Type spec.ObjectType
 }
 
-func (c *clg) Calculate(inputs []reflect.Value) ([]reflect.Value, error) {
-	outputs, err := filterError(reflect.ValueOf(c.calculate).Call(inputs))
+func (c *clg) Calculate(payload spec.NetworkPayload) (spec.NetworkPayload, error) {
+	outputs, err := filterError(reflect.ValueOf(c.calculate).Call(payload.Args))
 	if err != nil {
-		return nil, maskAny(err)
+		return spec.NetworkPayload{}, maskAny(err)
 	}
 
-	return outputs, nil
+	calculatedPayload := spec.NetworkPayload{
+		Args:        outputs,
+		Destination: "", // This has to be decided by Network.Forward.
+		Sources:     []spec.ObjectID{payload.Destination},
+	}
+
+	return calculatedPayload, nil
 }
 
 func (c *clg) GetName() string {
 	return "subtract"
 }
 
-func (c *clg) Inputs() []reflect.Type {
+func (c *clg) GetInputChannel() chan spec.NetworkPayload {
+	return c.InputChannel
+}
+
+func (c *clg) GetInputTypes() []reflect.Type {
 	t := reflect.TypeOf(c.calculate)
 
-	var clgInputs []reflect.Type
+	var inputType []reflect.Type
 
 	for i := 0; i < t.NumIn(); i++ {
-		clgInputs = append(clgInputs, t.In(i))
+		inputType = append(inputType, t.In(i))
 	}
 
-	return clgInputs
+	return inputType
 }
 
 func (c *clg) SetLog(log spec.Log) {
