bea1fb36d70a8168923dffde9ebb7160ff2fc5f7
Tim Schindler <xh3b4sd@gmail.com>
2016-08-16 22:37:15 +0000

    removed hard coded templates from clggen

--- clg/clg.go
+++ clg/clg.go
@@ -1,8 +1,9 @@
-// Package clg provides CLGs that implement basic behaviour TODO
+// Package clg provides CLGs that implement basic behaviour leveraged in the
+// neural network.
 //
 // Note that this package defines a go generate statement to generate fully
 // functional source code for all CLGs.
 //
-//go:generate ${GOPATH}/bin/clggen generate -i .
+//go:generate ${GOPATH}/bin/clggen generate --clg-dir=. --template-dir=../template
 //
 package clg
