ded47925d5a68cfa167e423ab641ffd6ac3eb239
Tim Schindler <xh3b4sd@gmail.com>
2016-08-17 23:44:55 +0000

    added object tests for generated CLGs

---
+++ clg/sum/generated_object_test.go
@@ -0,0 +1,23 @@
+package sum
+
+import (
+	"testing"
+)
+
+func Test_CLG_GetID(t *testing.T) {
+	firstCLG := MustNew()
+	secondCLG := MustNew()
+
+	if firstCLG.GetID() == secondCLG.GetID() {
+		t.Fatal("expected", false, "got", true)
+	}
+}
+
+func Test_CLG_GetType(t *testing.T) {
+	newCLG := MustNew()
+	objectType := newCLG.GetType()
+
+	if objectType != ObjectType {
+		t.Fatal("expected", ObjectType, "got", objectType)
+	}
+}
