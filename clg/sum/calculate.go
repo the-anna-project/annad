2a89c7f470d702a62353b71bc79f4697e6559c89
Tim Schindler <xh3b4sd@gmail.com>
2016-08-17 22:43:57 +0000

    added tests for sum CLG

--- clg/sum/calculate.go
+++ clg/sum/calculate.go
@@ -2,7 +2,11 @@
 // addition.
 package sum
 
+import (
+	"golang.org/x/net/context"
+)
+
 // calculate creates the sum of the given float64s.
-func (c *clg) calculate(a, b float64) float64 {
-	return a + b
+func (c *clg) calculate(ctx context.Context, a, b float64) (context.Context, float64) {
+	return ctx, a + b
 }
