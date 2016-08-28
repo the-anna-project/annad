25f74b2e9a66ca4ecc1a8d6cf307b69f6a0634e6
Tim Schindler <xh3b4sd@gmail.com>
2016-08-28 23:56:39 +0000

    tests

--- storage/redis/object_test.go
+++ storage/redis/object_test.go
@@ -5,8 +5,14 @@ import (
 )
 
 func Test_Storage_GetID(t *testing.T) {
-	firstStorage := testMaybeNewStorage(t)
-	secondStorage := testMaybeNewStorage(t)
+	firstStorage, err := NewStorage(DefaultStorageConfig())
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
+	secondStorage, err := NewStorage(DefaultStorageConfig())
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
 
 	if firstStorage.GetID() == secondStorage.GetID() {
 		t.Fatal("expected", "different IDs", "got", "equal IDs")
@@ -14,7 +20,10 @@ func Test_Storage_GetID(t *testing.T) {
 }
 
 func Test_Storage_GetType(t *testing.T) {
-	newStorage := testMaybeNewStorage(t)
+	newStorage, err := NewStorage(DefaultStorageConfig())
+	if err != nil {
+		t.Fatal("expected", nil, "got", err)
+	}
 
 	if newStorage.GetType() != ObjectType {
 		t.Fatal("expected", ObjectType, "got", newStorage.GetType())
