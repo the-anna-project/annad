29ac2bf0e6c75f10bad09b096f8102fa7e64b719
Tim Schindler <xh3b4sd@gmail.com>
2016-08-28 23:01:52 +0000

    golint

--- storage/error.go
+++ storage/error.go
@@ -5,6 +5,9 @@ import (
 	"github.com/xh3b4sd/anna/storage/redis"
 )
 
+// IsNotFound combines IsNotFound error matchers of all storage
+// implementations. IsNotFound should thus be used for error handling wherever
+// spec.Storage is dealt with.
 func IsNotFound(err error) bool {
 	return redis.IsNotFound(err) || memory.IsNotFound(err)
 }
