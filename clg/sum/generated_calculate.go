2946e123d4f6e6a370a8a7572cd8094952fe7a50
Tim Schindler <xh3b4sd@gmail.com>
2016-09-21 23:04:05 +0000

    removed TODO comments

--- clg/sum/generated_calculate.go
+++ clg/sum/generated_calculate.go
@@ -18,7 +18,6 @@ import (
 const (
 	// ObjectType represents the object type of the CLG object. This is used e.g.
 	// to register itself to the logger.
-	// TODO test
 	ObjectType spec.ObjectType = "sum-clg"
 )
 
