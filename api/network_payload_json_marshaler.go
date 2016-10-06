31af7609774b7b1c44df9eef0dde2f49347113fc
Tim Schindler <xh3b4sd@gmail.com>
2016-10-06 20:44:43 +0000

    added json Marshaler and Unmarshaler to spec.NetworkPayload

---
+++ api/network_payload_json_marshaler.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"encoding/json"
+)
+
+func (np *networkPayload) MarshalJSON() ([]byte, error) {
+	type NetworkPayloadClone networkPayload
+
+	b, err := json.Marshal(&struct {
+		*NetworkPayloadClone
+	}{
+		NetworkPayloadClone: (*NetworkPayloadClone)(np),
+	})
+	if err != nil {
+		return nil, maskAny(err)
+	}
+
+	return b, nil
+}
