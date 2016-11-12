9a74e4e0fe90e7ff37e6f1403df079c268312218
Tim Schindler <xh3b4sd@gmail.com>
2016-11-12 15:03:29 +0100

    removed index

--- index/index.go
+++
@@ -1,4 +0,0 @@
-// Package index groups entitiy implementations of the same type, e.g. the
-// entity CLG, provided by the clg package, implements an index providing CLGs
-// available to be used by e.g. networks.
-package index
