cae790f734f1e05baf6410a924500eec520671ba
Tim Schindler <xh3b4sd@gmail.com>
2016-11-15 00:38:00 +0100

    added more information to version command

--- command/version/command.go
+++ command/version/command.go
@@ -15,12 +15,20 @@ func New() *Command {
 type Command struct {
 	// Settings.
 
-	gitCommit string
+	gitCommit      string
+	goArch         string
+	goOS           string
+	goVersion      string
+	projectVersion string
 }
 
 // Execute represents the cobra run method.
 func (c *Command) Execute(cmd *cobra.Command, args []string) {
-	fmt.Printf("Git Commit: %s\n", c.gitCommit)
+	fmt.Printf("Git Commit:         %s\n", c.gitCommit)
+	fmt.Printf("Go Arch:            %s\n", c.goArch)
+	fmt.Printf("Go OS:              %s\n", c.goOS)
+	fmt.Printf("Go Version:         %s\n", c.goVersion)
+	fmt.Printf("Project Version:    %s\n", c.projectVersion)
 }
 
 // New creates a new cobra command for the version command.
@@ -38,4 +46,24 @@ func (c *Command) New() *cobra.Command {
 // SetGitCommit sets the git commit for the version command to be displayed.
 func (c *Command) SetGitCommit(gitCommit string) {
 	c.gitCommit = gitCommit
+}
+
+// SetGoArch sets the go architecture for the version command to be displayed.
+func (c *Command) SetGoArch(goArch string) {
+	c.goArch = goArch
+}
+
+// SetGoOS sets the go OS for the version command to be displayed.
+func (c *Command) SetGoOS(goOS string) {
+	c.goOS = goOS
+}
+
+// SetGoVersion sets the go version for the version command to be displayed.
+func (c *Command) SetGoVersion(goVersion string) {
+	c.goVersion = goVersion
+}
+
+// SetProjectVersion sets the project version for the version command to be displayed.
+func (c *Command) SetProjectVersion(projectVersion string) {
+	c.projectVersion = projectVersion
 }
