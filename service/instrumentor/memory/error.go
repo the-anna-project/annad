7464d5a593b6387b7064470acc15fa60032fee39
Tim Schindler <xh3b4sd@gmail.com>
2016-11-19 02:49:00 +0100

    removed local instrumentor service

--- service/instrumentor/memory/error.go
+++
@@ -1,9 +0,0 @@
-package memory
-
-import (
-	"github.com/juju/errgo"
-)
-
-var (
-	maskAny = errgo.MaskFunc(errgo.Any)
-)
--- service/instrumentor/prometheus/help.go
+++
@@ -1,9 +0,0 @@
-package prometheus
-
-import (
-	"fmt"
-)
-
-func helpFor(metricType, key string) string {
-	return fmt.Sprintf("This %s is represented by the metric name %s.", metricType, key)
-}
