ed50fcfcd19a96d1c99d2922b14823fb1fad3a51
Tim Schindler <xh3b4sd@gmail.com>
2016-11-20 03:23:49 +0100

    updated spec in vendor deps

--- vendor/github.com/the-anna-project/spec/object/permutation_list.go
+++ vendor/github.com/the-anna-project/spec/object/permutation_list.go
@@ -2,20 +2,18 @@ package spec
 
 // PermutationList is supposed to be permuted by a permutation service.
 type PermutationList interface {
-	// GetIndizes returns the list's current indizes.
-	GetIndizes() []int
-
-	// GetMaxGrowth returns the list's configured growth limit. The growth limit
+	// Indizes returns the list's current indizes.
+	Indizes() []int
+	// MaxGrowth returns the list's configured growth limit. The growth limit
 	// is used to prevent infinite permutations. E.g. MaxGrowth set to 4 will not
 	// permute up to a list of 5 raw values.
-	GetMaxGrowth() int
-
-	// GetPermutedValues returns the list's permuted values.
-	GetPermutedValues() []interface{}
-
-	// GetRawValues returns the list's unpermuted raw values.
-	GetRawValues() []interface{}
-
+	MaxGrowth() int
+	// PermutedValues returns the list's permuted values.
+	PermutedValues() []interface{}
+	// RawValues returns the list's unpermuted raw values.
+	RawValues() []interface{}
 	// SetIndizes sets the given indizes of the current permutation list.
 	SetIndizes(indizes []int)
+	SetMaxGrowth(maxGrowth int)
+	SetRawValues(rawValues []interface{})
 }
